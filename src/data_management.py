e0b59cc764472a868ffc327e8ca6c0b3eac815d9
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-11-25 12:58:08 +0000

    fix: fix st.cache data_management.py deployment error

--- src/data_management.py
+++ src/data_management.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-@st.cache_data()
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def loan_default_data():
     df = pd.read_csv("outputs/datasets/collection/row/LoanDefaultDataset.csv")
     return df
