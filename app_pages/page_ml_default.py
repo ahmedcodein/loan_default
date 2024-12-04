629d4f34912d62feb10a6fbbad0eef9e2b5c19cd
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-12-04 19:44:52 +0000

    docs: improve ml_default page text

--- app_pages/page_ml_default.py
+++ app_pages/page_ml_default.py
@@ -27,15 +27,17 @@ def ml_default_body():
     st.write("### ML Pipeline: Predict Default")
     # display pipeline training summary conclusions
     st.info(
-        f"* The pipeline was tuned aiming at least 0.85 precision on 'Default (0)' class, "
-        f"since we are interested in this project in detecting a potential\n"
-        f"debtor Default. \n"
+        f"* The pipeline was tuned aiming at least 0.85 precision on 'Default\n"
+        f"(0)' class, since we are interested in this project in "
+        f" detecting a potential debtor Default.\n"
         f"* The pipeline performance on train and test set is **1** and \n"
         f"**0.95**, respectively."
     )
 
     # show pipelines
     st.write("---")
+    st.write("### 1. Pipelines")
+    st.write("")
     st.write("#### There are 2 ML Pipelines arranged in series.")
 
     st.write(
@@ -43,25 +45,25 @@ def ml_default_body():
         f"> **Note**: No **cleaning** is included since the original dataset comes\n"
         f"already cleaned"
     )
-    st.write("##### Feature Engineering Pipeline")
+    st.write("##### 1.1. Feature Engineering Pipeline")
     st.write(default_pipe_dc_fe)
 
     st.write("* The second is for feature scaling and modelling.")
-    st.write("##### Feature Scaling and Modelling Pipeline")
+    st.write("##### 1.2. Feature Scaling and Modelling Pipeline")
     st.write(default_pipe_model)
 
     # show feature importance plot
     st.write("---")
-    st.write("* The features the model was trained and their importance.")
+    st.write(
+        "* The features the model was trained on ranked based on their"
+        " importance.")
     st.write(X_train.columns.to_list())
     st.image(default_feat_importance)
 
-    # We don't need to apply dc_fe pipeline, since X_train and X_test
-    # were already transformed in the jupyter notebook (Predict Customer Churn.ipynb)
-
     # evaluate performance on train and test set
     st.write("---")
-    st.write("### Pipeline Performance")
+    st.write("### 2. Pipeline Performance")
+    st.write("")
     clf_performance(X_train=X_train, y_train=y_train,
                     X_test=X_test, y_test=y_test,
                     pipeline=default_pipe_model,
